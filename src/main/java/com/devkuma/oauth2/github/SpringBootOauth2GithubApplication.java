79f98f51cc11f0ae5f99396dd120127547a6e7f2
kimkc <redfreek2c@gmail.com>
2020-11-15 16:32:15 +0900

    init

---
+++ src/main/java/com/devkuma/oauth2/github/SpringBootOauth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.devkuma.oauth2.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2GithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2GithubApplication.class, args);
+	}
+
+}
